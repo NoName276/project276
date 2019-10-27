90135ffc7853623dc323e14a89da11438776697f
Colin Kwok <colinkwok@Colins-MacBook-Pro.local>
2019-10-26 23:45:40 -0700

    fixed hello page

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/hello', (req, res) => res.render('Hello There!'))
+  .get('/hello', (req, res) => res.send('Hello There!'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
