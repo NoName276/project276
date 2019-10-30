da37d4eff9d34446343364756741a912239f7f40
Quiette <55117989+Quiette@users.noreply.github.com>
2019-10-30 11:06:55 -0700

    testing submission

--- index.js
+++ index.js
@@ -8,5 +8,45 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/hello', (req, res) => res.send('Hello There!'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+app.get('/register', async (req, res) => {  //loads registerform
+    try {
+        res.render('pages/register', name);
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+});
+
+app.get('/login', async (req, res) => {     //loads loginform
+    try {
+        res.render('pages/login', name);
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+});
+
+app.get('/:user/login', async (req, res) => {   //puts info into databse and loads home
+        //  TODO
+    try {
+        res.render('pages/login', name);
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+});
+
+app.get('/:user/register', async (req, res) => {    //checks if person in databse with same password, home if does, error if not right password
+        // TODO
+    try {
+        res.render('pages/login', name);
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+});
+
+
 
