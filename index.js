602c07c9bc9a0e2db188525310ca37ccf126a5ce
Colin Kwok <colinkwok@Colins-MacBook-Pro.local>
2019-10-30 21:17:42 -0700

    express app is now defined in index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/hello', (req, res) => res.send('Hello There!'))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+var app = express()
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/hello', (req, res) => res.send('Hello There!'))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 app.get('/register', async (req, res) => {  //loads registerform
     try {
