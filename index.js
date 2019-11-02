e72021131dd8af46b496752ba5f8c4fd20335e81
Noble Tan <nobletanx@gmail.com>
2019-11-01 20:36:45 -0700

    update

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 
-// TODO: UN COMMENT. database not setup yet.
-// var pool = new Pool({
-//   connectionString: process.env.DATABASE_URL
-// });
-var app = express()
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
-app.get('/hello', (req, res) => res.send('Hello There!'))
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+var pool = new Pool({
+    ssl: true,
+    connectionString:""
+});
+var app = express();
+app.use(express.urlencoded());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+app.get('/hello', (req, res) => res.send('Hello There!'));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 app.post('/club/reg', (req,res) => {        // loads new reg to database +check if username already exist
     console.log(req.body);
