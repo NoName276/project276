b0c491521ec081005580facd2ed555f369b6f7c5
Jarrod Vanthournout <jvanthou@sfu.ca>
2019-11-03 19:55:39 -0800

    prep for merge

--- index.js
+++ index.js
@@ -2,16 +2,6 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-<<<<<<< HEAD
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/hello', (req, res) => res.send('Hello There!'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-//test
-=======
 var app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
@@ -59,5 +49,4 @@ app.get('/:user/register', async (req, res) => {    //checks if person in databs
 });
 
 
->>>>>>> 9b825e0a180a25a8fde9edef445c1d5982b1f700
 
