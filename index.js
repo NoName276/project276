1445c8b226b23df5ad0619dfe086e2421efd1e38
Colin Kwok <colinkwok@Colins-MacBook-Pro.local>
2019-11-05 00:16:08 -0800

    access token retreival

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
 var SpotifyWebApi = require('spotify-web-api-node');
+const PORT = process.env.PORT || 5000
 
 var app = express()
 app.use(express.static(path.join(__dirname, 'public')))
