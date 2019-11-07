e43f7c354d88c44517b57ddaa2d6704b8fb1675a
Colin Kwok <colinkwok@Colins-MacBook-Pro.local>
2019-11-06 20:17:13 -0800

    successful api call to get track tempo and duration

--- index.js
+++ index.js
@@ -62,3 +62,21 @@ app.get('/song', (req,res) => {
     res.render('pages/song');
 })
 
+// credentials are optional
+var spotifyApi = new SpotifyWebApi({
+    clientId: '76399d6d66784fbd9b089a5363553e47',
+    clientSecret: '5d6ec7245f5a4902af2f5b40c6315a63',
+    redirectUri: 'http://localhost:5000/song'
+});
+
+spotifyApi.setAccessToken('BQDRiH_GrXe9m_lE5sFXrg6dvwlpsSphZwVTYxv7LBM0JO9Hg5h4N0ru8CHH-7kFX9zragIOyDNAHhHRrSdyx1AFxRfukMz22IZ3rnY2xVHjoVod09rXDx5TvR9-VQhtsStDRR1QzlmzsZFlm9MHjpRXMPCX78-MwkYXYb2K4mKE5fcUy9foMSQTzQ');
+
+/* Get Audio Analysis for a Track */
+spotifyApi.getAudioAnalysisForTrack('0rKtyWc8bvkriBthvHKY8d')
+  .then(function(data) {
+    console.log(data.body.track.duration);
+    console.log(data.body.track.tempo);
+  }, function(err) {
+    done(err);
+});
+  
