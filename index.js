80ea47daaebc72462101972b448f90eaa2215e89
Colin Kwok <colinkwok@Colins-MacBook-Pro.local>
2019-12-01 01:20:39 -0800

    drop down menu for songs in room and mp3 integrated with game

--- index.js
+++ index.js
@@ -11,12 +11,6 @@ server.listen(PORT, () => {
   console.log(`Express App and Socket IO server listing on PORT ${PORT}`)
 });
 
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-server.listen(PORT, () => {
-  console.log(`Express App and Socket IO server listing on PORT ${PORT}`)
-});
-
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json());
@@ -40,7 +34,7 @@ app.get('/register', async (req, res) => {  //loads registerform
 })
 var pool = new Pool({
   ssl: true,
-  //connectionString: process.env.DATABASE_URL
+//   connectionString: process.env.DATABASE_URL
   connectionString: "postgres://onmhemgydrtawp:44340bfdc255d71d386e984a35a34725a508b67d94cc356653fc8aa407264744@ec2-174-129-252-252.compute-1.amazonaws.com:5432/dad64i7292eb5o"
 });
 // var app = express();
@@ -384,7 +378,7 @@ app.post('/playing', (req,res) => {
                 queryData.duration = data.body.track.duration;
                 queryData.tempo = data.body.track.tempo;
                 // res.send(queryData)
-
+                queryData.track = "silence"
                 // res.render('pages/playing', queryData)
             res.render('pages/game', queryData)
             }, function(err) {
@@ -400,6 +394,73 @@ app.post('/playing', (req,res) => {
     });
 })
 
+app.post('/getSong', (req,res)=> {
+    var songData = {}
+    console.log("hello")
+    console.log(req.body.songs)
+    songData.track = req.body.songs
+    var durationDict = {
+        "slow-dancing-in-the-dark": 209,
+        "breathe": 126,
+        "bad-guy": 194,
+        "dont-start-now": 183,
+        "how-do-you-sleep": 202,
+        "its-you": 214,
+        "dear-evan-hansen": 236,
+        "hadestown": 230
+
+    }
+    var tempoDict = {
+        "slow-dancing-in-the-dark": 89,
+        "breathe": 113,
+        "bad-guy": 135,
+        "dont-start-now": 124,
+        "how-do-you-sleep": 111,
+        "its-you": 96,
+        "dear-evan-hansen": 144,
+        "hadestown": 120
+    }
+    var artistDict = {
+        "slow-dancing-in-the-dark": "Joji",
+        "breathe": "88rising, Joji, Don Krez",
+        "bad-guy": "Billie Eilish",
+        "dont-start-now": "Dua Lipa",
+        "how-do-you-sleep": "Sam Smith",
+        "its-you": "Ali Gatie",
+        "dear-evan-hansen": "DEAR EVAN HANSEN Original Broadway Cast",
+        "hadestown": "Hadestown Original Broadway Cast"
+    }
+    var nameDict = {
+        "slow-dancing-in-the-dark": "SLOW DANCING IN THE DARK",
+        "breathe": "Breathe",
+        "bad-guy": "bad guy",
+        "dont-start-now": "Don't Start Now",
+        "how-do-you-sleep": "How Do You Sleep?",
+        "its-you": "It's You",
+        "dear-evan-hansen": "Waving Through a Window",
+        "hadestown": "Way down Hadestown II"
+    }
+    var uriDict = {
+        "slow-dancing-in-the-dark": "0rKtyWc8bvkriBthvHKY8d",
+        "breathe": "72hSRAHg4hXE5ApwSQQZPn",
+        "bad-guy": "2Fxmhks0bxGSBdJ92vM42m",
+        "dont-start-now": "6WrI0LAC5M1Rw2MnX2ZvEg",
+        "how-do-you-sleep": "spotify:track:6b2RcmUt1g9N9mQ3CbjX2Y",
+        "its-you": "5DqdesEfbRyOlSS3Tf6c29",
+        "dear-evan-hansen": "2GlsXuQV0YOOwPy3XKFIS9",
+        "hadestown": "1FomWvYH5RqagHVHYj9OfC"
+    }
+    songData.duration = durationDict[songData.track]
+    songData.tempo = tempoDict[songData.track]
+    songData.artist = artistDict[songData.track]
+    songData.name = nameDict[songData.track]
+    songData.uri = uriDict[songData.track]
+    songData.accessToken = 0 
+    res.render('pages/game', songData)
+})
+app.get('/silence.mp3', (req,res) => {
+    res.sendFile(__dirname + '/audio/silence.mp3')
+})
 app.get('/joji', (req,res) => {
     res.render('pages/slow-dancing-in-the-dark');
 })
@@ -409,14 +470,14 @@ app.get('/slow-dancing-in-the-dark.mp3', (req,res) => {
 app.get('/hadestown', (req, res) => {
     res.render('pages/way-down-hadestown-ii');
 })
-app.get('/hadestown-original-broadway-cast-way-down-hadestown-ii-lyrics.mp3', (req,res) => {
-    res.sendFile(__dirname + '/audio/hadestown-original-broadway-cast-way-down-hadestown-ii-lyrics.mp3')
+app.get('/hadestown.mp3', (req,res) => {
+    res.sendFile(__dirname + '/audio/hadestown.mp3')
 })
 app.get('/dear-evan-hansen', (req,res) => {
     res.render('pages/waving-through-a-window');
 })
-app.get('/waving-through-a-window-from-the-dear-evan-hansen-original-broadway-cast-recording.mp3', (req,res) => {
-    res.sendFile(__dirname + '/audio/waving-through-a-window-from-the-dear-evan-hansen-original-broadway-cast-recording.mp3')
+app.get('/waving-through-a-window.mp3', (req,res) => {
+    res.sendFile(__dirname + '/audio/waving-through-a-window.mp3')
 })
 app.get('/88rising', (req,res) => {
     res.render('pages/breathe');
