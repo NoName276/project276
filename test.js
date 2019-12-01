9f364d871baacf13e454846856f5a99fbf1fd9ab
Colin Kwok <colinkwok@Colins-MacBook-Pro.local>
2019-11-30 16:46:57 -0800

    all tests pass

--- test.js
+++ test.js
@@ -276,7 +276,7 @@ describe("Stats and Leaderboard", function () {
                     //console.log(res.text);
                     res.text.should.include("Welcome, Quiette. Here is the Leaderboard:"); //in leaderboard
                     res.text.should.include("BobbyC"); //holds top player
-                    res.text.should.include("Vanthournout");//holds 10th place player or lowest player if less than ten 
+                    res.text.should.include("jen");//holds 10th place player or lowest player if less than ten 
                     res.text.should.include("Here is your standing:");
                     res.text.should.include("Quiette"); //holds self in table
                     done();
